61015208a7a04c82ebcaf4ed5f40f37dbb8ae7cd
ploutarchosm <info@ploutarchosm.com>
2022-06-08 13:45:45 +0300

    import service

--- src/application/app.module.ts
+++ src/application/app.module.ts
@@ -3,6 +3,7 @@ import { APP_FILTER } from '@nestjs/core';
 import { AppController } from '../controllers/app.controller';
 import { HttpExceptionFilter } from '../services/exception.filter.service';
 import { AppConfigModule } from '../config/app.config.module';
+import { ApplicationService } from '../services/app.service';
 
 @Module({
   imports: [AppConfigModule],
